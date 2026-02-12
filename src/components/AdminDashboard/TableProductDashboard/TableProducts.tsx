1125ae2c0c3a27817ddc48510277df2511e6a591
javiercolodro <javiercolodro@gmail.com>
2026-02-11 22:34:28 -0300

    fix limit product stock colour

--- src/components/AdminDashboard/TableProductDashboard/TableProducts.tsx
+++ src/components/AdminDashboard/TableProductDashboard/TableProducts.tsx
@@ -159,7 +159,7 @@ const TableProducts = ({ products }: TableProductsProps) => {
                   className={`p-2 rounded-full text-sm font-semibold whitespace-nowrap  ${
                     product.stock === 0
                       ? "bg-red-100 text-red-700"
-                      : product.stock < 10
+                      : product.stock <= 5
                         ? "bg-orange-100 text-orange-700"
                         : "bg-green-100 text-green-700"
                   }`}
